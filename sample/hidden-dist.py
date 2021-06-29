59a80a979a4695f4fa0076dc984dd54c68991df3
Tim Chen <timzai@gmail.com>
2021-06-29 21:42:14 +0800

    remove unused code from sample

--- sample/hidden-dist.py
+++ sample/hidden-dist.py
@@ -1,44 +1,6 @@
-# Keep this file separate
-
-# https://www.pg4e.com/code/hidden-dist.py
-
-# psql -h pg.pg4e.com -p 5432 -U pg4e_be9e729093 pg4e_be9e729093
-
-# %load_ext sql
-# %config SqlMagic.autocommit=False
-# %sql postgresql://pg4e_be9e729093:pg4e_p_d5fab7440699124@pg.pg4e.com:5432/pg4e_be9e729093
-# %sql SELECT 1 as "Test"
-
 def secrets():
     return {"host": "",
             "port": 5432,
             "database": "",
             "user": "",
             "pass": ""}
-
-
-# Return a psycopg2 connection string
-
-# import hidden
-# secrets = hidden.readonly()
-# sql_string = hidden.psycopg2(hidden.readonly())
-
-# 'dbname=pg4e_data user=pg4e_data_read password=pg4e_p_d5fab7440699124 host=pg.pg4e.com port=5432'
-
-def psycopg2(secrets) :
-     return ('dbname='+secrets['database']+' user='+secrets['user']+
-        ' password='+secrets['pass']+' host='+secrets['host']+
-        ' port='+str(secrets['port']))
-
-# Return an SQLAlchemy string
-
-# import hidden
-# secrets = hidden.readonly()
-# sql_string = hidden.alchemy(hidden.readonly())
-
-# postgresql://pg4e_data_read:pg4e_p_d5fab7440699124@pg.pg4e.com:5432/pg4e_data
-
-def alchemy(secrets) :
-    return ('postgresql://'+secrets['user']+':'+secrets['pass']+'@'+secrets['host']+
-        ':'+str(secrets['port'])+'/'+secrets['database'])
-
