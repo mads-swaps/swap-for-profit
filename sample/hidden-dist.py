2fdf0872c915acb3b9c6cdf64d31282ace6a2a3c
Tim Chen <timzai@gmail.com>
2021-06-29 21:43:05 +0800

    oops i deleted too much

--- sample/hidden-dist.py
+++ sample/hidden-dist.py
@@ -4,3 +4,8 @@ def secrets():
             "database": "",
             "user": "",
             "pass": ""}
+
+def psycopg2(secrets) :
+     return ('dbname='+secrets['database']+' user='+secrets['user']+
+        ' password='+secrets['pass']+' host='+secrets['host']+
+        ' port='+str(secrets['port']))
