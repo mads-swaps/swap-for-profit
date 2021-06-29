a3f747e0ba2681534b9d05949bcbb7262fa6e4d0
Tim Chen <timzai@gmail.com>
2021-06-30 02:35:54 +0800

    RDS DB schema + data

---
+++ rds/initial_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO pairs (id,symbol,coin1,coin2) VALUES
+	 (0,'ETHBTC','ETH','BTC'),
+	 (1,'BTCUSDT','BTC','USDT'),
+	 (2,'ETHUSDT','ETH','USDT');
---
+++ rds/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE pairs (
+	id int2 NOT NULL,
+	symbol varchar NOT NULL,
+	coin1 varchar NOT NULL,
+	coin2 varchar NOT NULL,
+	CONSTRAINT pairs_pk PRIMARY KEY (id)
+);
+
+
+CREATE TABLE candlestick_15m (
+	pair_id int2 NOT NULL,
+	open_time timestamptz NOT NULL,
+	"open" numeric(15, 8) NOT NULL,
+	high numeric(15, 8) NOT NULL,
+	low numeric(15, 8) NOT NULL,
+	"close" numeric(15, 8) NOT NULL,
+	volume numeric(15, 8) NOT NULL,
+	close_time timestamptz NOT NULL,
+	quote_asset_volume numeric(15, 8) NOT NULL,
+	number_of_trades int4 NOT NULL,
+	taker_buy_base_asset_volume numeric(15, 8) NOT NULL,
+	taker_buy_quote_asset_volume numeric(15, 8) NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now(),
+	CONSTRAINT candlestick_15m_pair_open_time_key UNIQUE (pair_id, open_time),
+	CONSTRAINT candlestick_15m_fk FOREIGN KEY (pair_id) REFERENCES pairs(id)
+);
+CREATE UNIQUE INDEX candlestick_15m_pair_id_idx ON candlestick_15m USING btree (pair_id, open_time);
