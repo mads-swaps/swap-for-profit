6394340a2830417fee5b5f403fa007881ad2b63d
Tim Chen <timzai@gmail.com>
2021-07-11 04:57:05 +0800

    Simulation notebook to get the logic down

--- aws/sam/mads-swap-app/swap_simulation/buy_decision/dummy.py
+++ aws/sam/mads-swap-app/swap_simulation/buy_decision/dummy.py
@@ -5,9 +5,6 @@ def columns():
 
 def make_decision(x_data = [2,3,1,2.1]):
     # return true if buy
-
-
     # open / high / low / close
-
     # if close is higher than open, buy!
-    return x_data[3] > x_data[0]
+    return (x_data[:,3] > (x_data[:,0]*1.005)).reshape(-1,)
--- aws/sam/mads-swap-app/swap_simulation/price_decision/dummy.py
+++ aws/sam/mads-swap-app/swap_simulation/price_decision/dummy.py
@@ -1,9 +1,9 @@
 # dummy
+import numpy as np
 
 def columns():
     return ['open', 'high', 'low', 'close']
 
 def stop_loss_target_range(x_data = [2,3,1,2.1]):
     # return tuple of (stop-loss, sell target)
-    current_price = x_data[0]
-    return (current_price * 0.95, current_price * 1.05)
+    return np.array([x_data[:, 0] * 0.95, x_data[:, 0] * 1.05]).T
