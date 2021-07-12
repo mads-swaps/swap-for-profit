eb2cb7281fd3412b3e736d4ebb9587947c1e40e2
Tim Chen <timzai@gmail.com>
2021-07-12 23:01:03 +0800

    Fixed incorrect labels for stop loss and target (oops)

--- aws/sam/mads-swap-app/swap_simulation/price_decision/dummy.py
+++ aws/sam/mads-swap-app/swap_simulation/price_decision/dummy.py
@@ -5,5 +5,5 @@ def columns():
     return ['open', 'high', 'low', 'close']
 
 def stop_loss_target_range(x_data = [2,3,1,2.1]):
-    # return tuple of (stop-loss, sell target)
+    # return tuple of (sell target, stop-loss)
     return np.array([x_data[:, 0] * 0.95, x_data[:, 0] * 1.05]).T
