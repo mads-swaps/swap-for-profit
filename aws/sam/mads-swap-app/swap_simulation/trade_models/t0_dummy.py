62f165458f7277574fdde50f1b75224f977c355b
Tim Chen <timzai@gmail.com>
2021-07-29 02:49:37 +0800

    Let there be extra rows

--- aws/sam/mads-swap-app/swap_simulation/trade_models/t0_dummy.py
+++ aws/sam/mads-swap-app/swap_simulation/trade_models/t0_dummy.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 
 def columns():
-    return ['open', 'close']
+    return ['close']
 
-def make_decision(x_data, info_dict):
+def make_decision(x_data, extra_data, info_dict):
     # you can assign key and values to info_dict to pass data between batches
     # ie: if you do info_dict['test'] = 'testing', next batch will have access to info_dict['test']
     # portfolio is provided by default
@@ -18,12 +18,18 @@ def make_decision(x_data, info_dict):
 
     
     # Remember to do scaling or normalization here if needed. x_data is not scaled or normalized
-    
-    delta = x_data['close'] - x_data['open']
+
+    # since we have 2 extra rows, we can do something with it in extra_data
+    extra_data['custom_close_2'] = extra_data['close'].shift(2)
+    # assign it to x_data using the "is_extra" flag so the extra data isn't used
+    x_data['custom_close_2'] = extra_data[~extra_data['is_extra']]['custom_close_2']
+
+    # now it can be used
+    delta = x_data['close'] - x_data['custom_close_2']
     
     decision = pd.Series(0, index=x_data.index) # hold
-    decision = decision.where(delta > x_data['open'] * 0.005, 1) # buy if close is went up more than 0.5%
-    decision = decision.where(-delta > x_data['open'] * 0.005, -1) # sell if close is went down more than 0.5%
+    decision = decision.where(delta > x_data['custom_close_2'] * 0.01, 1) # buy if close is went up more than 1%
+    decision = decision.where(-delta > x_data['custom_close_2'] * 0.01, -1) # sell if close is went down more than 1%
     
     # now I want to add a restriction that if I bought or sold, I will hold for at least 8 candles
     # I'll go back and change buy/sell decisions if they are within 8 candles of last buy/sell
